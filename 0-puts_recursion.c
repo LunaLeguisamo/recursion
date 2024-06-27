c12f23e160c0471ec9af56e058d13b2ebac59461
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-27 14:02:52 +0000

    0-puts_recursion

---
+++ 0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  function that prints a string,
+ * followed by a new line
+ * Return: The string to print
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	
+	_putchar('\n');
+}
